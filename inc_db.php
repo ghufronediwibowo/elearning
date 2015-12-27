207f11b61659a5dbd28337e43826c286226289d5
ghufronediwibowo <ghufronedi@gmail.com>
2015-12-27 13:57:13 +0700

    versi php sedang diupdate

--- inc_db.php
+++ inc_db.php
@@ -4,7 +4,7 @@ $server = "localhost";
 $username = "root";
 $password = "";
 $database = "elearning";
-// Koneksi dan memilih database di server
+// Koneksi dan memilih database di server php 7
 @mysql_connect($server,$username,$password) or die("Koneksi gagal");
 @mysql_select_db($database) or die("Database tidak bisa dibuka");
 
